c9a2be6d6c165bef21f65c7c3e1e6bdf74a76065
gabriel.mitzka <gabriel.mitzka@CPX-PBNR8UTOAZ6.dir.svc.accenture.com>
2019-08-08 15:25:15 +0200

    initial commit

---
+++ myprotest1/src/com/acn/HelloJumpers.java
@@ -0,0 +1,10 @@
+package com.acn;
+
+public class HelloJumpers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello Jumpstarters");
+	}
+
+}
